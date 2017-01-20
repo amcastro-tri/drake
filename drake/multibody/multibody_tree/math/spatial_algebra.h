bb5b3d6173118c9bdd45ca4cb413060a6fea7e5a
amcastro-tri <alejandro.castro@tri.global>
2017-01-20 14:16:13 -0500

    Implements SpatialVelocityJacobian

--- drake/multibody/multibody_tree/math/spatial_algebra.h
+++ drake/multibody/multibody_tree/math/spatial_algebra.h
@@ -13,14 +13,6 @@ namespace drake {
 namespace multibody {
 namespace math {
 
-#if 0
-/// A column vector of size 6 consisting of a concatenating rotational and
-/// translational components of a physical quantity, see
-/// @ref multibody_spatial_algebra
-template<typename Scalar>
-using SpatialVector = Eigen::Matrix<Scalar, 6, 1>;
-#endif
-
 /// Given the spatial velocity `V_AB` of a frame `B` with respect to a frame `A`,
 /// compute the spatial velocity of a frame `Q` rigidly moving with `B` but
 /// offset by vector `r_BA`.
@@ -134,6 +126,52 @@ std::ostream& operator<<(std::ostream& o, const SpatialVector<T>& V) {
   return o;
 }
 
+/// Multiplication from the left by a scalar.
+template <typename T>
+inline SpatialVector<T> operator*(
+    const T& s, const SpatialVector<T>& V)
+{
+  return SpatialVector<T>(s * V.angular(), s * V.linear());
+}
+
+/// Class representing the Hinge matrix...
+/// Essentially a matrix containing a SpatialVector in the column corresponding
+/// to a generalized velocity.
+template <typename T, int ndofs>
+class SpatialVelocityJacobian {
+ public:
+  enum {
+    kSpatialVectorSize = 6,
+    kSpatialVectorAngularSize = 3,
+    kSpatialVectorLinearSize = 3
+  };
+  typedef Eigen::Matrix<T, kSpatialVectorSize, ndofs> CoeffsEigenType;
+
+  int rows() const { return J_.rows(); }
+  int cols() const { return J_.cols(); }
+
+  const T& operator()(int i, int j) const { return J_(i, j);}
+
+  const SpatialVector<T>& col(int i) const {
+    DRAKE_ASSERT(0 < i && i < cols());
+    // Assumes column major order. Eigen's default.
+    return *reinterpret_cast<const SpatialVector<T>*>(&(J_.col(i)[0]));
+  }
+
+  SpatialVector<T>& col(int i) {
+    DRAKE_ASSERT(0 < i && i < cols());
+    // Assumes column major order. Eigen's default.
+    return *reinterpret_cast<SpatialVector<T>*>(&(J_.col(i)[0]));
+  }
+
+  const CoeffsEigenType& get_coeffs() const { return J_;}
+
+  CoeffsEigenType& get_mutable_coeffs() { return J_;}
+
+ private:
+  CoeffsEigenType J_;
+};
+
 // Forward declaration of the ShiftOperator's transpose.
 template <typename T> class ShiftOperatorTranspose;
 
@@ -156,7 +194,7 @@ class ShiftOperator {
 
   // Returns the vector from the origin of frame X to the origin of frame Y
   // expressed in the implicit frame F.
-  const Vector3<T>& offset_XoYo_F() const { return offset_XoYo_F_; }
+  const Vector3<T>& offset() const { return offset_XoYo_F_; }
 
   ShiftOperatorTranspose<T> transpose() const;
 
@@ -172,23 +210,32 @@ public:
 
   // Returns the vector from the origin of frame X to the origin of frame Y
   // expressed in the implicit frame F.
-  const Vector3<T>& offset_XoYo_F() const { return phi_XY_F_.offset_XoYo_F(); }
+  const Vector3<T>& offset() const { return phi_XY_F_.offset(); }
 
-  /// Given the spatial velocity `V_AB` of a frame `B` with respect to a frame `A`,
-  /// compute the spatial velocity of a frame `Q` rigidly moving with `B` but
-  /// offset by vector `r_BA`.
-  /// @param[in] V_AB Spatial velocity of frame `B` with respect to frame `A`,
-  /// expressed in frame `A`.
-  /// @param[in] r_BQ_A Shift vector from `Bo` to `Qo`, expressed in frame `A`.
-  /// @returns V_AQ_A The spatial velocity of frame `Q` with respect to `A` and
+  /// Given the spatial velocity `V_AB` of a frame `B` with respect to a frame 
+  /// `A`, compute the spatial velocity of a frame `Q` rigidly moving with `B` 
+  /// but offset by vector `r_BQ`.
+  ///
+  /// The operation performed, in vector free form, is:
+  ///   w_AQ = w_AB,  i.e. the angular velocity of frame B and Q is the same.
+  ///   v_AQ = v_AB + w_AB x r_BQ
+  ///
+  /// All quantities above must be expressed in the same frame `E` i.e:
+  ///   w_AQ_E = w_AB_E
+  ///   v_AQ_E = v_AB_E + w_AB_E x r_BQ_E
+  ///
+  /// @param[in] V_AB_E Spatial velocity of frame `B` with respect to frame `A`,
+  /// expressed in frame `E`.
+  /// @param[in] r_BQ_E Shift vector from `Bo` to `Qo`, expressed in frame `E`.
+  /// @returns V_AQ_E The spatial velocity of frame `Q` with respect to `A` and
   /// expressed in frame `A`.
   static SpatialVector<T> ShiftSpatialVelocity(
-      const SpatialVector<T>& V_AB, const Vector3<T>& r_BQ_A) {
+      const SpatialVector<T>& V_AB_E, const Vector3<T>& r_BQ_E) {
     return SpatialVector<T>(
         /* Same angular velocity. */
-        V_AB.angular(),
+        V_AB_E.angular(),
         /* Linear velocity v_AQ = v_AB + w_AB.cross(r_BQ). */
-        V_AB.linear() + V_AB.angular().cross(r_BQ_A));
+        V_AB_E.linear() + V_AB_E.angular().cross(r_BQ_E));
   }
 
  private:
@@ -200,16 +247,63 @@ inline ShiftOperatorTranspose<T> ShiftOperator<T>::transpose() const {
   return ShiftOperatorTranspose<T>(*this);
 }
 
-/// @param[in] phiT_XY_F Transpose of the shift operator from frame X to Y
-/// expressed in a frame F.
-/// @param[in] V_FX The spatial velocity of frame X measured and expressed in F.
-/// @returns V_FY The spatial velocity of frame Y measured and expressed in F.
+/// Given the spatial velocity `V_AB` of a frame `B` measured in a frame `A`, 
+/// compute the spatial velocity of a frame `Q` rigidly moving with `B` 
+/// but offset by vector `r_BQ`.
+///
+/// The operation performed, in vector free form, is:
+///   V_AQ = phi_BQ^T * V_AB
+///
+/// All quantities above must be expressed in the same frame `E` i.e:
+///   V_AQ_E = phi_BQ_E^T * V_AB_E
+///
+/// @param[in] phiT_BQ_E Transpose of the shift operator from frame `B` to `Q`
+/// expressed in a frame `E`.
+/// @param[in] V_AB_E The spatial velocity of frame `B` measured in `A` 
+/// and expressed in `E`.
+/// @returns V_AQ_E The spatial velocity of frame `Q` measured in `A` and 
+/// expressed in `E`.
 template <typename T>
 inline SpatialVector<T> operator*(
-    const ShiftOperatorTranspose<T>& phiT_XY_F, const SpatialVector<T>& V_FX)
+    const ShiftOperatorTranspose<T>& phiT_BQ_E, const SpatialVector<T>& V_AB_E)
 {
   return ShiftOperatorTranspose<T>::ShiftSpatialVelocity(
-      V_FX, phiT_XY_F.offset_XoYo_F());
+      V_AB_E, phiT_BQ_E.offset());
+}
+
+/// @param[in] phiT_AB_F Transpose of the shift operator from frame A to B
+/// expressed in a frame F.
+/// @param[in] H_FA A spatial velocity Jacobian with each column representing a
+/// spatial velocity of frame A measured and expressed in frame F.
+/// @returns H_FB The spatial velocity jacobain for frame B measured and
+/// expressed in F.
+template <typename T, int ndofs>
+inline SpatialVelocityJacobian<T, ndofs> operator*(
+    const ShiftOperatorTranspose<T>& phiT_BQ_E,
+    const SpatialVelocityJacobian<T, ndofs>& J_AB_E)
+{
+  SpatialVelocityJacobian<T, ndofs> J_AQ_E;
+  auto& Eigen_J_AQ_E = J_AQ_E.get_mutable_coeffs();
+  const auto& Eigen_J_AB_E = J_AB_E.get_coeffs();
+  // Same angular velocity.
+  Eigen_J_AQ_E.template topRows<3>() = Eigen_J_AB_E.template topRows<3>();
+  // Linear velocity v_AQ = v_AB + w_AB.cross(r_BQ).
+  Eigen_J_AQ_E.template bottomRows<3>().noalias() =
+      Eigen_J_AB_E.template bottomRows<3>() +
+          Eigen_J_AB_E.template topRows<3>().colwise().cross(
+              phiT_BQ_E.offset());
+  return J_AQ_E;
+}
+
+template <typename T, int ndofs> inline
+std::ostream& operator<<(std::ostream& o,
+                         const SpatialVelocityJacobian<T, ndofs>& J) {
+  for(int i = 0; i < J.rows(); ++i) {
+    o << "[" << J(i, 0);
+    for (int j = 1; j < J.cols(); ++j) o << " | " << J(i, j);
+    o << "]" << std::endl;
+  }
+  return o;
 }
 
 }  // math

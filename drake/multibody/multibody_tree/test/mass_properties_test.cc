ab3ffc7a0ab0a62b0dc80490b141c73d7614fe6b
amcastro-tri <alejandro.castro@tri.global>
2017-01-25 16:30:47 -0500

    Implements SptialInertia::operator+=() and a few others

--- drake/multibody/multibody_tree/test/mass_properties_test.cc
+++ drake/multibody/multibody_tree/test/mass_properties_test.cc
@@ -79,8 +79,69 @@ GTEST_TEST(RotationalInertia, ReExpressInAnotherFrame) {
   PRINT_VARn(I_Ro_F);
 
   // Check if after transformation this still is a physically valid inertia.
-  EXPECT_TRUE(I_Ro_F.IsValidRotationalInertia());
+  EXPECT_TRUE(I_Ro_F.IsPhysicallyValid());
 
+}
+
+GTEST_TEST(SpatialInertia, PlusEqualOperator) {
+  const double L = 2.0;
+  // Rod frame R located at the rod's geometric center and oriented along its
+  // principal axes.
+  // Inertia computed about Ro and expressed in R.
+
+  // Spatial inertia computed about the origin for a cube with sides of
+  // length 2.0 centered at x = 1.0. Expressed in world frame.
+  const double mass_right = 1.5;
+  SpatialInertia<double> MRightBox_Wo_W(
+      mass_right,
+      Vector3d::Zero(),
+      mass_right * RotationalInertia<double>::SolidCube(L));
+  MRightBox_Wo_W.ShiftOriginInPlace(-Vector3d::UnitX());
+  // Check if after transformation this still is a physically valid inertia.
+  EXPECT_TRUE(MRightBox_Wo_W.IsPhysicallyValid());
+
+  PRINT_VARn(MRightBox_Wo_W);
+
+  // Spatial inertia computed about the origin for a cube with sides of
+  // length 2.0 centered at x = -1.0. Expressed in world frame.
+  const double mass_left = 0.5;
+  SpatialInertia<double> MLeftBox_Wo_W(
+      mass_left,
+      Vector3d::Zero(),
+      mass_left * RotationalInertia<double>::SolidCube(L));
+  MLeftBox_Wo_W.ShiftOriginInPlace(Vector3d::UnitX());
+  // Check if after transformation this still is a physically valid inertia.
+  EXPECT_TRUE(MLeftBox_Wo_W.IsPhysicallyValid());
+
+  PRINT_VARn(MLeftBox_Wo_W);
+
+  // Spatial inertia of a prism with a squared transverse area of size
+  // 2.0 x 2.0 and length 4.0.
+  // This is computed by adding the above spatial inertias.
+  // Notice that the origina and the expressed-in frame is the same as in the
+  // two individual components.
+  SpatialInertia<double> MPrism_Wo_W(MLeftBox_Wo_W);
+  MPrism_Wo_W += MRightBox_Wo_W;
+  EXPECT_TRUE(MPrism_Wo_W.IsPhysicallyValid());
+
+  PRINT_VARn(MPrism_Wo_W);
+
+  // Check that the compound inertia corresponds to that of a larger box of
+  // length 4.0.
+  const double mass = mass_left + mass_right;
+  const Vector3d com(
+      (mass_left * MLeftBox_Wo_W.get_com() +
+       mass_right * MRightBox_Wo_W.get_com()) / mass);
+  SpatialInertia<double> MExpected_Wo_W(
+      mass,
+      com,
+      mass * RotationalInertia<double>::SolidBox(2 * L, L, L));
+  // Check if after transformation this still is a physically valid inertia.
+  EXPECT_TRUE(MExpected_Wo_W.IsPhysicallyValid());
+
+  PRINT_VARn(MExpected_Wo_W);
+
+  EXPECT_TRUE(MPrism_Wo_W.IsApprox(MExpected_Wo_W));
 }
 
 GTEST_TEST(SpatialInertia, ReExpress) {
@@ -101,7 +162,7 @@ GTEST_TEST(SpatialInertia, ReExpress) {
   SpatialInertia<double> M_Bo_W = M_Bo_B.ReExpress(R_WB);
 
   // Checks for physically correct spatial inertia.
-  EXPECT_TRUE(M_Bo_W.IsValidSpatialInertia());
+  EXPECT_TRUE(M_Bo_W.IsPhysicallyValid());
 
   // Replace this print by the respective checks of the new computed values.
   // What was in y now is in z.
@@ -115,11 +176,12 @@ GTEST_TEST(SpatialInertia, ShiftOrigin) {
 
   // Spatial inertia for a thin rod of unit mass computed about its center of
   // mass and expressed in the world frame W.
-  const double mass = 1.0;
+  const double mass = 1.2;
   const double radius = 0.05, length = 1.0;
-  SpatialInertia<double> M_Bo_W = SpatialInertia<double>(
+  SpatialInertia<double> M_Bo_W(
       mass, Vector3d::Zero(),
-      RotationalInertia<double>::SolidRod(radius, length)).ReExpress(R_WB);
+      mass * RotationalInertia<double>::SolidRod(radius, length));
+  M_Bo_W.ReExpressInPlace(R_WB);
 
   // Replace this print by the respective getters and check values.
   PRINT_VARn(M_Bo_W);
@@ -137,7 +199,7 @@ GTEST_TEST(SpatialInertia, ShiftOrigin) {
   PRINT_VARn(M_Xo_W);
 
   // Checks for physically correct spatial inertia.
-  EXPECT_TRUE(M_Xo_W.IsValidSpatialInertia());
+  EXPECT_TRUE(M_Xo_W.IsPhysicallyValid());
 
   // Expected moment of inertia for a rod when computed about one of its ends.
   const double I_end = mass * length / 3.0;

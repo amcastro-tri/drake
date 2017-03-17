db31da4015954827017db1c6ee027494b9b457ad
Sean Curtis <sean.curtis@tri.global>
2017-03-17 15:58:35 -0700

    Initial geometry_channel implementation
    The vast majority of the implementation does *nothing*.  Merely returns
    types.

---
+++ drake/geometry/geometry_channel.cc
@@ -0,0 +1,71 @@
+#include "drake/geometry/geometry_channel.h"
+
+#include "drake/common/drake_assert.h"
+#include "drake/geometry/geometry_instance.h"
+#include "drake/geometry/geometry_world.h"
+
+namespace drake {
+namespace geometry {
+
+using drake::systems::Context;
+using std::unique_ptr;
+
+template <typename T>
+GeometryChannel<T>::GeometryChannel(ChannelId index,
+                                    GeometryWorld<T>* geometry_world)
+    : id_(index), geometry_world_(geometry_world) {
+  DRAKE_ASSERT(geometry_world != nullptr);
+}
+
+template <typename T>
+GeometryChannel<T>::~GeometryChannel() {
+  DRAKE_DEMAND(geometry_world_ == nullptr);
+}
+
+template <typename T>
+void GeometryChannel<T>::Close() {
+  // TODO(SeanCurtis-TRI): Do the work on geometry world's side to close the
+  // channel.
+  geometry_world_ = nullptr;
+}
+
+template <typename T>
+FrameId GeometryChannel<T>::DeclareFrame(Context<T>* context) {
+  // TODO(SeanCurtis-TRI): Do the work!
+  static int NEXT_FRAME = 1;
+  return FrameId(NEXT_FRAME++);
+}
+
+template <typename T>
+GeometryId GeometryChannel<T>::DeclareGeometry(Context<T>* context,
+                                            FrameId id,
+                                            unique_ptr<GeometryInstance> geometry,
+                                            const Isometry3<T>& X_FG) {
+  // TODO(SeanCurtis-TRI): Do the work!
+  static int NEXT_GEOMETRY = 1;
+  return GeometryId(NEXT_GEOMETRY++);
+}
+
+template <typename T>
+GeometryId GeometryChannel<T>::DeclareGeometry(Context<T>* context,
+                                            GeometryId id,
+                                            unique_ptr<GeometryInstance> geometry,
+                                            const Isometry3<T>& X_FG) {
+  // TODO(SeanCurtis-TRI): Do the work!
+  return DeclareGeometry(context, FrameId(0), geometry, X_FG);
+}
+
+template <typename T>
+FrameKinematicsSet<T> GeometryChannel<T>::GetFrameKinematicsSet() {
+  FrameKinematicsSet<T> set(id_);
+  // TODO(SeanCurtis-TRI): Populate this from the reference data.
+  return set;
+}
+
+template <typename T>
+void GeometryChannel<T>::UpdateFrameKinematicsSet(const Context<T>& context,
+                                                  FrameKinematicsSet<T>* frame_set) {
+  // TODO(SeanCurtis-TRI): Do this work.
+}
+}  // namespace geometry
+}  // namespace drake

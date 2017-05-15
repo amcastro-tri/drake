a91fbb7836b63e57f6fda1fbf9964291c7c77623
Sean Curtis <sean.curtis@tri.global>
2017-05-15 11:28:43 -0700

    Modify GeometryEngine to return GeometryId values
    In order to reduce the number of O(N) operations on query results, the
    engine maps its internal indices into global ids via a provided look-up
    table.

---
+++ drake/geometry/geometry_query_results.h
@@ -0,0 +1,103 @@
+#pragma once
+
+#include "drake/common/drake_copyable.h"
+#include "drake/common/eigen_types.h"
+#include "drake/geometry/geometry_ids.h"
+
+namespace drake {
+namespace geometry {
+
+// TODO(SeanCurtis-TRI): Include the objects involved -- GeometryFrameIds?
+//    Determine what the query primitive is: the GeometryFrameId?  Contact
+//    element?  Visual element?  Pointer?  Id?  Etc.
+//    This applies to all of these return types.
+
+/** The data for a single contact between two bodies/elements.
+ @tparam T The underlying scalar type. Must be a valid Eigen scalar. */
+template <typename T>
+struct Contact {
+  /** The point of contact in the world frame. */
+  Vector3<T> point_W;
+
+  /** The contact normal in the world frame. */
+  Vector3<T> normal_W;
+
+  /** The penetration distance. */
+  T distance{};
+};
+
+/** The data for reporting the distance between two geometries, A and B.
+ @tparam T The underlying scalar type. Must be a valid Eigen scalar. */
+template <typename T>
+struct NearestPair {
+  DRAKE_DEFAULT_COPY_AND_MOVE_AND_ASSIGN(NearestPair)
+
+  NearestPair() {}
+
+  /** Constructor
+   @param a       The id of the first geometry (A).
+   @param b       The id of the second geometry (B).
+   @param p_A     The point on geometry A's surface nearest B, in A's frame.
+   @param p_B     The point on geometry B's surface nearest A, in B's frame.
+   @param dist    The distance between p_A and p_B. */
+  NearestPair(GeometryId a, GeometryId b, const Vector3<T>& p_A,
+              const Vector3<T>& p_B, T dist) : id_A(a), id_B(b),
+                                               p_ACa(p_A), p_BCb(p_B),
+                                               distance(dist) {}
+
+  /** The id of the first geometry in the pair. */
+  GeometryId id_A;
+  /** The id of the second geometry in the pair. */
+  GeometryId id_B;
+  /** The point on geometry A's surface nearest B, in A's frame. */
+  Vector3<T> p_ACa;
+  /** The point on geometry B's surface nearest A, in B's frame. */
+  Vector3<T> p_BCb;
+  /** The distance between p_A_A and p_B_B (measured in a common frame). */
+  T distance{};
+};
+
+/** The data for reporting the geometry nearest a query point. The struct does
+ not store the value of the query point, but assumes the user will correlate the
+ result with the query previously provided query point.
+ @tparam T The underlying scalar type. Must be a valid Eigen scalar. */
+template <typename T>
+struct PointProximity {
+  DRAKE_DEFAULT_COPY_AND_MOVE_AND_ASSIGN(PointProximity)
+
+  PointProximity() {}
+  /** Constructor
+   @param id              The id of the near geometry (named "A").
+   @param local_point     The point on A's surface near the query point (the
+                          proximal point), in A's frame.
+   @param world_point     The proximal point, in the world frame.
+   @param world_dir       A unit-length vector pointing from the proximal
+                          point to the query point, in the world frame.
+   @param dist            The distance from the query point to the proximal
+                          point. */
+  PointProximity(GeometryId id, const Vector3<T>& local_point,
+                 const Vector3<T>& world_point, const Vector3<T>& world_dir,
+                 T dist)
+      : id_A(id),
+        p_ACa(local_point),
+        p_WCa(world_point),
+        rhat_CaQ_W(world_dir),
+        distance(dist) {}
+  /** The id of the near geometry, named "A". */
+  GeometryId id_A;
+  /** The point on A's surface nearest the query point, in A's frame. */
+  Vector3<T> p_ACa;
+  /** The point on A's surface nearest the query point, in the world frame. */
+  Vector3<T> p_WCa;
+  /** An _optional_ unit-length vector indicating the direction from the point
+   on A's surface to the query point Q (measured and expressed in the world
+   frame). This only contains a vector if the query point does _not_ lie on the
+   surface (i.e., |distance| > epsilon). */
+  optional<Vector3<T>> rhat_CaQ_W;
+  /** The *signed* distance between p_ACa and the query point (negative values
+   imply the point lies *inside* the surface). */
+  T distance{};
+};
+
+}  // namespace geometry
+}  // namespace drake
--- drake/geometry/test/geometry_engine_stub_test.cc
+++ drake/geometry/test/geometry_engine_stub_test.cc
@@ -28,18 +28,18 @@ using GEngine = GeometryEngineStub<double>;
 // as a key in a set or map as long as the set/map is declared with this as
 // the class as its own Hash class. E.g.,
 //  unordered_map<IndexPair, float, IndexPair> pair_to_scalar_;
-struct IndexPair {
-  GeometryIndex idx1;
-  GeometryIndex idx2;
-  IndexPair() {}
-  IndexPair(GeometryIndex i1, GeometryIndex i2) : idx1(i1), idx2(i2) {}
-  bool operator==(const IndexPair& other) const {
-    return idx1 == other.idx1 && idx2 == other.idx2;
+struct IdPair {
+  GeometryId id1;
+  GeometryId id2;
+  IdPair() {}
+  IdPair(GeometryId i1, GeometryId i2) : id1(i1), id2(i2) {}
+  bool operator==(const IdPair& other) const {
+    return id1 == other.id1 && id2 == other.id2;
   }
   // The pair serves is its own hash function.
-  size_t operator()(const IndexPair& p) const noexcept {
-    size_t key = p.idx1;
-    return key << 32 | p.idx2;
+  size_t operator()(const IdPair& p) const noexcept {
+    size_t key = static_cast<size_t>(p.id1.get_value());
+    return key << 32 | static_cast<size_t>(p.id2.get_value());
   }
 };
 
@@ -54,6 +54,7 @@ class GeometryEngineStubTest : public ::testing::Test {
     // between them.
     for (int i = 0; i < kSphereCount; ++i) {
       axis_indices_.push_back(engine_.AddDynamicGeometry(make_sphere(kRadius)));
+      sphere_ids_.push_back(GeometryId::get_new_id());
     }
 
     // Position them at origin, [1, 0, 0], [0, 1, 0], and [0, 0, 1],
@@ -76,65 +77,69 @@ class GeometryEngineStubTest : public ::testing::Test {
     engine_.UpdateWorldPoses(poses);
 
     const double sqrt2 = sqrt(2);
-    near_points_[IndexPair(axis_indices_[0], axis_indices_[1])] =
+    near_points_[IdPair(sphere_ids_[0], sphere_ids_[1])] =
         make_pair(Vector3<double>(0.5, 0, 0), Vector3<double>(-0.5, 0, 0));
-    near_points_[IndexPair(axis_indices_[0], axis_indices_[2])] =
+    near_points_[IdPair(sphere_ids_[0], sphere_ids_[2])] =
         make_pair(Vector3<double>(0, 0, -0.5), Vector3<double>(0, -0.5, 0));
-    near_points_[IndexPair(axis_indices_[0], axis_indices_[3])] =
+    near_points_[IdPair(sphere_ids_[0], sphere_ids_[3])] =
         make_pair(Vector3<double>(0, 0.5, 0), Vector3<double>(0, 0, -0.5));
-    near_points_[IndexPair(axis_indices_[1], axis_indices_[2])] =
+    near_points_[IdPair(sphere_ids_[1], sphere_ids_[2])] =
         make_pair(Vector3<double>(-sqrt2 / 2, sqrt2 / 2, 0) * kRadius,
                   Vector3<double>(sqrt2 / 2, -sqrt2 / 2, 0) * kRadius);
-    near_points_[IndexPair(axis_indices_[1], axis_indices_[3])] =
+    near_points_[IdPair(sphere_ids_[1], sphere_ids_[3])] =
         make_pair(Vector3<double>(-sqrt2 / 2, 0, sqrt2 / 2) * kRadius,
                   Vector3<double>(sqrt2 / 2, 0, -sqrt2 / 2) * kRadius);
-    near_points_[IndexPair(axis_indices_[2], axis_indices_[3])] =
+    near_points_[IdPair(sphere_ids_[2], sphere_ids_[3])] =
         make_pair(Vector3<double>(0, -sqrt2 / 2, sqrt2 / 2) * kRadius,
                   Vector3<double>(0, sqrt2 / 2, -sqrt2 / 2) * kRadius);
 
-    distances_[IndexPair(axis_indices_[0], axis_indices_[1])] = 0;
-    distances_[IndexPair(axis_indices_[0], axis_indices_[2])] = 0;
-    distances_[IndexPair(axis_indices_[0], axis_indices_[3])] = 0;
-    distances_[IndexPair(axis_indices_[1], axis_indices_[2])] = sqrt2 - 1;
-    distances_[IndexPair(axis_indices_[1], axis_indices_[3])] = sqrt2 - 1;
-    distances_[IndexPair(axis_indices_[2], axis_indices_[3])] = sqrt2 - 1;
+    distances_[IdPair(sphere_ids_[0], sphere_ids_[1])] = 0;
+    distances_[IdPair(sphere_ids_[0], sphere_ids_[2])] = 0;
+    distances_[IdPair(sphere_ids_[0], sphere_ids_[3])] = 0;
+    distances_[IdPair(sphere_ids_[1], sphere_ids_[2])] = sqrt2 - 1;
+    distances_[IdPair(sphere_ids_[1], sphere_ids_[3])] = sqrt2 - 1;
+    distances_[IdPair(sphere_ids_[2], sphere_ids_[3])] = sqrt2 - 1;
   }
 
   // A collection of methods to assert correctness of results. Confirms that the
   // nearest pair data is consistent when considering the first *n* spheres.
   void ExpectCorrectProximity(
-      const vector<internal::NearestPair<double>>& results,
-      const vector<IndexPair>& pairs) {
+      const vector<NearestPair<double>>& results,
+      const vector<IdPair>& pairs) {
     EXPECT_EQ(results.size(), pairs.size());
 
     const double kTolerance = 1e-14;
     for (int i = 0; i < static_cast<int>(pairs.size()); ++i) {
       const auto& pair = pairs[i];
       const auto& data = results[i];
-      EXPECT_EQ(data.index_A, pair.idx1);
-      EXPECT_EQ(data.index_B, pair.idx2);
+      EXPECT_EQ(data.id_A, pair.id1);
+      EXPECT_EQ(data.id_B, pair.id2);
       EXPECT_EQ(data.distance, distances_[pair]);
       EXPECT_TRUE(
-          CompareMatrices(data.p_A_A, near_points_[pair].first, kTolerance));
+          CompareMatrices(data.p_ACa, near_points_[pair].first, kTolerance));
       EXPECT_TRUE(
-          CompareMatrices(data.p_B_B, near_points_[pair].second, kTolerance));
+          CompareMatrices(data.p_BCb, near_points_[pair].second, kTolerance));
     }
   }
 
   GEngine engine_;
   // Geometry indices for the axis-sphere configuration.
-  static const float kRadius;
+  static const double kRadius;
   static const int kSphereCount;
   // The indices of the spheres added to the engine.
   vector<GeometryIndex> axis_indices_;
+  // The ith entry contains the id that corresponds to the ith geometry in
+  // axis_indices_;
+  vector<GeometryId> sphere_ids_;
   // The position of the near point on sphere i, for the pair (i, j), using
   // GetKeyFromIndices to map (i, j) into the key. The point on j, nearest i,
   // is just the negative value.
-  unordered_map<IndexPair, std::pair<Vector3<double>, Vector3<double>>, IndexPair> near_points_;
-  unordered_map<IndexPair, double, IndexPair> distances_;
+  unordered_map<IdPair, std::pair<Vector3<double>, Vector3<double>>, IdPair>
+      near_points_;
+  unordered_map<IdPair, double, IdPair> distances_;
 };
 
-const float GeometryEngineStubTest::kRadius = 0.5f;
+const double GeometryEngineStubTest::kRadius = 0.5;
 const int GeometryEngineStubTest::kSphereCount = 4;
 
 TEST_F(GeometryEngineStubTest, Constructor) {
@@ -144,14 +149,14 @@ TEST_F(GeometryEngineStubTest, Constructor) {
 
 TEST_F(GeometryEngineStubTest, ComputePairwiseClosestPoints_All) {
   SetUpAxisSpheres();
-  std::vector<internal::NearestPair<double>> results;
-  EXPECT_TRUE(engine_.ComputePairwiseClosestPoints(&results));
-  vector<IndexPair> computed_pairs = { {axis_indices_[0], axis_indices_[1]},
-                                       {axis_indices_[0], axis_indices_[2]},
-                                       {axis_indices_[0], axis_indices_[3]},
-                                       {axis_indices_[1], axis_indices_[2]},
-                                       {axis_indices_[1], axis_indices_[3]},
-                                       {axis_indices_[2], axis_indices_[3]}};
+  std::vector<NearestPair<double>> results;
+  EXPECT_TRUE(engine_.ComputePairwiseClosestPoints(sphere_ids_, &results));
+  vector<IdPair> computed_pairs = {{sphere_ids_[0], sphere_ids_[1]},
+                                   {sphere_ids_[0], sphere_ids_[2]},
+                                   {sphere_ids_[0], sphere_ids_[3]},
+                                   {sphere_ids_[1], sphere_ids_[2]},
+                                   {sphere_ids_[1], sphere_ids_[3]},
+                                   {sphere_ids_[2], sphere_ids_[3]}};
   ExpectCorrectProximity(results, computed_pairs);
 }
 
@@ -160,11 +165,12 @@ TEST_F(GeometryEngineStubTest, ComputePairwiseClosestPoints_SelectIndices) {
   vector<GeometryIndex> query_indices = {axis_indices_[0],
                                          axis_indices_[2],
                                          axis_indices_[3]};
-  std::vector<internal::NearestPair<double>> pairs;
-  EXPECT_TRUE(engine_.ComputePairwiseClosestPoints(query_indices, &pairs));
-  vector<IndexPair> computed_pairs = { {axis_indices_[0], axis_indices_[2]},
-                                       {axis_indices_[0], axis_indices_[3]},
-                                       {axis_indices_[2], axis_indices_[3]}};
+  std::vector<NearestPair<double>> pairs;
+  EXPECT_TRUE(engine_.ComputePairwiseClosestPoints(
+      sphere_ids_, query_indices, &pairs));
+  vector<IdPair> computed_pairs = {{sphere_ids_[0], sphere_ids_[2]},
+                                   {sphere_ids_[0], sphere_ids_[3]},
+                                   {sphere_ids_[2], sphere_ids_[3]}};
   ExpectCorrectProximity(pairs, computed_pairs);
 }
 
@@ -173,10 +179,11 @@ TEST_F(GeometryEngineStubTest, ComputePairwiseClosestPoints_SelectPairs) {
   vector<internal::GeometryIndexPair> query_pairs;
   query_pairs.emplace_back(axis_indices_[0], axis_indices_[1]);
   query_pairs.emplace_back(axis_indices_[2], axis_indices_[3]);
-  std::vector<internal::NearestPair<double>> results;
-  EXPECT_TRUE(engine_.ComputePairwiseClosestPoints(query_pairs, &results));
-  vector<IndexPair> computed_pairs = { {axis_indices_[0], axis_indices_[1]},
-                                       {axis_indices_[2], axis_indices_[3]}};
+  std::vector<NearestPair<double>> results;
+  EXPECT_TRUE(engine_.ComputePairwiseClosestPoints(
+      sphere_ids_, query_pairs, &results));
+  vector<IdPair> computed_pairs = {{sphere_ids_[0], sphere_ids_[1]},
+                                   {sphere_ids_[2], sphere_ids_[3]}};
   ExpectCorrectProximity(results, computed_pairs);
 }
 
@@ -187,42 +194,41 @@ TEST_F(GeometryEngineStubTest, FindClosestGeometry) {
   points.resize(3, kPointCount);
   SetUpAxisSpheres();
 
-  vector<internal::PointProximity<double>> results;
-  vector<internal::PointProximity<double>> expected;
+  vector<PointProximity<double>> results;
+  vector<PointProximity<double>> expected;
 
   // Point 0: Directly below sphere 0 at the origin. Because of 0's rotation
   // the world and local nearest points are different.
   points.block<3, 1>(0, 0) << 0, 0, -1;
-  expected.emplace_back(axis_indices_[0],
+  expected.push_back(PointProximity<double>(sphere_ids_[0],
                        Vector3<double>(0, -kRadius, 0),
                        Vector3<double>(0, 0, -kRadius),
                        Vector3<double>(0, 0, -1),
-                       0.5);
+                       0.5));
 
   // Point 1: To the right of sphere 1 (the sphere at <1, 0, 0>).
   points.block<3, 1>(0, 1) << 2, 0, 0;
-  expected.emplace_back(axis_indices_[1],
+  expected.push_back(PointProximity<double>(sphere_ids_[1],
                         Vector3<double>(kRadius, 0, 0),
                         Vector3<double>(kRadius + 1, 0, 0),
                         Vector3<double>(1, 0 ,0),
-                        0.5);
+                        0.5));
 
   // Point 2: Lies *slightly* inside sphere 2 (the sphere at <0, 1, 0>).
   const double kInset = 0.1;
   points.block<3, 1>(0, 2) << 0, 1 + kRadius - kInset, 0;
-  expected.emplace_back(axis_indices_[2],
+  expected.push_back(PointProximity<double>(sphere_ids_[2],
                         Vector3<double>(0, kRadius, 0),
                         Vector3<double>(0, 1 + kRadius, 0),
                         Vector3<double>(0, -1, 0),
-                        -kInset);
+                        -kInset));
 
-  EXPECT_TRUE(engine_.FindClosestGeometry(points, &results));
+  EXPECT_TRUE(engine_.FindClosestGeometry(sphere_ids_, points, &results));
   EXPECT_EQ(results.size(), expected.size());
   for (int i = 0; i < kPointCount; ++i) {
-    EXPECT_EQ(results[i].index_A, expected[i].index_A);
+    EXPECT_EQ(results[i].id_A, expected[i].id_A);
   }
 }
-
 }  // namespace
 }  // namespace geometry
 }  // namespace drake

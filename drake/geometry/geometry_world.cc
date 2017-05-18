75400514affa11958ec247e01b2a1643db0a94eb
Sean Curtis <sean.curtis@tri.global>
2017-05-18 09:56:12 -0700

    Decouple GeometryContext from GeometryWorld
    GeometryWorld should not directly depend on Context. The Context is a
    system construct.  The GeometryState is what GeometryWorld cares about.
    It is what is stored inside a context.  But the System should be responsible
    for extracting it from the context and providing it to the world.
    
    - Refactor source
    - Correct tests
    - Downstream implications

--- drake/geometry/geometry_world.cc
+++ drake/geometry/geometry_world.cc
@@ -20,118 +20,82 @@ using std::unique_ptr;
 using std::vector;
 
 template <typename T>
-SourceId GeometryWorld<T>::RegisterNewSource(GeometryContext<T>* context,
+SourceId GeometryWorld<T>::RegisterNewSource(GeometryState<T>* state,
                                              const std::string& name) {
   using std::to_string;
   SourceId id = SourceId::get_new_id();
-  auto& state = context->get_mutable_state()
-      ->template get_mutable_abstract_state<GeometryState<T>>(0);
-  state.RegisterNewSource(id);
   std::string source_name = name == "" ? "Source_" + to_string(id) : name;
-  for (auto pair : sources_) {
-    if (pair.second == source_name) {
-      throw std::logic_error(
-          "Registering new source with duplicate name: " + source_name + ".");
-    }
-  }
-  sources_[id] = source_name;
+  state->RegisterNewSource(id, source_name);
   return id;
 }
 
 template <typename T>
-bool GeometryWorld<T>::SourceIsRegistered(SourceId id) const {
-  return sources_.find(id) != sources_.end();
+bool GeometryWorld<T>::SourceIsRegistered(const GeometryState<T>& state,
+                                          SourceId id) const {
+  return state.source_is_active(id);
 }
 
 template <typename T>
-FrameId GeometryWorld<T>::RegisterFrame(GeometryContext<T>* context,
+FrameId GeometryWorld<T>::RegisterFrame(GeometryState<T>* state,
                                         SourceId source_id,
                                         const GeometryFrame<T>& frame) {
-  auto state = get_mutable_state(context);
   FrameId id = state->RegisterFrame(source_id, frame);
-  // TODO(SeanCurtis-TRI): Instantiate frame in data and handle eventual
-  // payload.
   return id;
 }
 
 template <typename T>
 GeometryId GeometryWorld<T>::RegisterGeometry(
-    GeometryContext<T>* context, SourceId source_id, FrameId frame_id,
+    GeometryState<T>* state, SourceId source_id, FrameId frame_id,
     unique_ptr<GeometryInstance<T>> geometry) {
-  auto state = get_mutable_state(context);
   return state->RegisterGeometry(source_id, frame_id, move(geometry));
 }
 
 template <typename T>
 GeometryId GeometryWorld<T>::RegisterGeometry(
-    GeometryContext<T>* context, SourceId source_id, GeometryId geometry_id,
+    GeometryState<T>* state, SourceId source_id, GeometryId geometry_id,
     unique_ptr<GeometryInstance<T>> geometry) {
-  auto state = get_mutable_state(context);
   return state->RegisterGeometryWithParent(source_id, geometry_id,
                                           std::move(geometry));
 }
 
 template <typename T>
 GeometryId GeometryWorld<T>::RegisterAnchoredGeometry(
-    GeometryContext<T>* context, SourceId source_id,
+    GeometryState<T>* state, SourceId source_id,
     unique_ptr<GeometryInstance<T>> geometry) {
-  auto state = get_mutable_state(context);
   return state->RegisterAnchoredGeometry(source_id, std::move(geometry));
 }
 
 template <typename T>
-void GeometryWorld<T>::ClearSource(GeometryContext<T>* context,
+void GeometryWorld<T>::ClearSource(GeometryState<T>* state,
                                    SourceId source_id) {
-  auto state = get_mutable_state(context);
   state->ClearSource(source_id);
 }
 
 template <typename T>
 FrameKinematicsSet<T> GeometryWorld<T>::GetFrameKinematicsSet(
-    SourceId source_id) {
-  DRAKE_ASSERT_VOID(AssertValidSource(source_id));
+    const GeometryState<T>& state, SourceId source_id) {
+  DRAKE_ASSERT_VOID(AssertValidSource(state, source_id));
   FrameKinematicsSet<T> set(source_id);
   return set;
 }
 
 template <typename T>
 void GeometryWorld<T>::SetFrameKinematics(
-    GeometryContext<T>* context,
+    GeometryState<T>* state,
     const FrameKinematicsSet<T>& frame_kinematics) {
-  auto state = get_state(*context);
-  state.SetFrameKinematics(frame_kinematics);
+  state->SetFrameKinematics(frame_kinematics);
 }
 
 template <typename T>
-GeometryQuery<T> GeometryWorld<T>::GetQuery(const GeometryContext<T>& context) const {
-  auto state = get_state(context);
-  return state.GetQuery();
+unique_ptr<GeometryState<T>> GeometryWorld<T>::CreateState() {
+  return make_unique<GeometryState<T>>();
 }
 
 template <typename T>
-vector<unique_ptr<AbstractValue>> GeometryWorld<T>::AllocateAbstractValues() {
-  vector<unique_ptr<AbstractValue>> values;
-  values.push_back(make_unique<Value<GeometryState<T>>>());
-  return values;
-}
-
-template <typename T>
-GeometryState<T>* GeometryWorld<T>::get_mutable_state(
-    GeometryContext<T>* context) {
-  return &context->get_mutable_state()
-              ->template get_mutable_abstract_state<GeometryState<T>>(0);
-}
-
-template <typename T>
-const GeometryState<T>& GeometryWorld<T>::get_state(
-    const GeometryContext<T>& context) const {
-  return context.get_state().template get_abstract_state<GeometryState<T>>(0);
-}
-
-template <typename T>
-void GeometryWorld<T>::AssertValidSource(SourceId source_id) const {
+void GeometryWorld<T>::AssertValidSource(const GeometryState<T>& state,
+                                         SourceId source_id) const {
   using std::to_string;
-  if (!SourceIsRegistered(source_id)) {
+  if (!state.source_is_active(source_id)) {
     throw std::logic_error("Invalid source id: " + to_string(source_id) + ".");
   }
 }
--- drake/geometry/test/geometry_world_test.cc
+++ drake/geometry/test/geometry_world_test.cc
@@ -4,7 +4,6 @@
 
 #include <gtest/gtest.h>
 
-#include "drake/geometry/geometry_context.h"
 #include "drake/geometry/geometry_instance.h"
 #include "drake/geometry/geometry_state.h"
 #include "drake/geometry/test/expect_error_message.h"
@@ -14,7 +13,6 @@ namespace geometry {
 namespace {
 
 using drake::Isometry3;
-using drake::systems::AbstractValues;
 using std::make_unique;
 using std::move;
 using std::unique_ptr;
@@ -23,29 +21,22 @@ class GeometryWorldTest : public ::testing::Test {
  protected:
   void SetUp() {
     world_ = make_unique<GeometryWorld<double>>();
-    context_ = make_unique<GeometryContext<double>>();
-    auto abstract_values = world_->AllocateAbstractValues();
-    context_->set_abstract_state(
-        make_unique<AbstractValues>(move(abstract_values)));
-    geometry_state_ =
-        &context_->get_mutable_abstract_state<GeometryState<double>>(0);
+    geometry_state_ = world_->CreateState();
   }
 
   // Members owned by the test class.
   unique_ptr<GeometryWorld<double>> world_;
-  unique_ptr<GeometryContext<double>> context_;
-  // The GeometryState instance is actually owned by the context; this is simply
-  // a convenience handle.
-  GeometryState<double>* geometry_state_;
+  unique_ptr<GeometryState<double>> geometry_state_;
 };
 
-// Confirm that the state is extracted from the context without any copying.
-TEST_F(GeometryWorldTest, PullStateFromContext) {
-  auto state_ptr = world_->get_mutable_state(context_.get());
-  EXPECT_EQ(state_ptr, geometry_state_);
-  auto& state = world_->get_state(*context_);
-  EXPECT_EQ(&state, geometry_state_);
-}
+// TODO(SeanCurtis-TRI): Move this into the GeometrySystem test.
+//// Confirm that the state is extracted from the context without any copying.
+//TEST_F(GeometryWorldTest, PullStateFromContext) {
+//  auto state_ptr = world_->get_mutable_state(context_.get());
+//  EXPECT_EQ(state_ptr, geometry_state_);
+//  auto& state = world_->get_state(*context_);
+//  EXPECT_EQ(&state, geometry_state_);
+//}
 
 // Confirms the behavior of creating a FrameKinematicsSet from a bad source id.
 // In release build, this is allowed (it'll throw an exception later when the
@@ -55,7 +46,7 @@ TEST_F(GeometryWorldTest, CreateFrameKinematicsSetFromBadSource) {
   SourceId s_id = SourceId::get_new_id();
 #ifdef DRAKE_ASSERT_IS_DISARMED
   // In release mode, this would be considered valid. No exception thrown.
-  auto fks = world_->GetFrameKinematicsSet(s_id);
+  auto fks = world_->GetFrameKinematicsSet(*geometry_state_, s_id);
 #else
   EXPECT_ERROR_MESSAGE(world_->GetFrameKinematicsSet(s_id),
                        std::logic_error,
@@ -63,29 +54,23 @@ TEST_F(GeometryWorldTest, CreateFrameKinematicsSetFromBadSource) {
 #endif
 }
 
-// Confirms the registration of the source with a user-specified name, a default
-// name, and requesting the name for an invalid id.
+// Confirms the registration of a new source with and without "default" names.
+// This does not test error conditions as that is handled in
+// geometry_state_test.
 TEST_F(GeometryWorldTest, TestSourceNames) {
   using std::to_string;
 
+  GeometryState<double>* state = geometry_state_.get();
+
   // Case: user-specified source name.
   std::string name = "my_source";
-  SourceId named_id = world_->RegisterNewSource(context_.get(), name);
-  EXPECT_EQ(world_->get_source_name(named_id), name);
+  SourceId named_id = world_->RegisterNewSource(state, name);
+  EXPECT_EQ(world_->get_source_name(*state, named_id), name);
 
   // Case: default name.
-  SourceId anon_id = world_->RegisterNewSource(context_.get());
-  EXPECT_EQ(world_->get_source_name(anon_id), "Source_" + to_string(anon_id));
-
-  // Case: duplicate name.
-  EXPECT_ERROR_MESSAGE(world_->RegisterNewSource(context_.get(), name),
-  std::logic_error,
-  "Registering new source with duplicate name: .+\\.");
-
-  // Case: query with invalid source id.
-  EXPECT_ERROR_MESSAGE(world_->get_source_name(SourceId::get_new_id()),
-                       std::logic_error,
-                       "Querying source name for an invalid source id: \\d+.");
+  SourceId anon_id = world_->RegisterNewSource(state);
+  EXPECT_EQ(world_->get_source_name(*state, anon_id),
+            "Source_" + to_string(anon_id));
 }
 
 }  // namespace

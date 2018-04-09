ef991a6803e542ca8531a9d95cc07f1f03184a11
amcastro-tri <alejandro.castro@tri.global>
2018-04-09 17:25:57 -0300

    multibody_tree_context compiles with DRAKE_DEFINE_CLASS_TEMPLATE_INSTANTIATIONS_ON_DEFAULT_SCALARS

--- multibody/multibody_tree/velocity_kinematics_cache.cc
+++ multibody/multibody_tree/velocity_kinematics_cache.cc
@@ -1,13 +1,6 @@
 #include "drake/multibody/multibody_tree/velocity_kinematics_cache.h"
 
-#include "drake/common/autodiff.h"
+#include "drake/common/default_scalars.h"
 
-namespace drake {
-namespace multibody {
-
-// Explicitly instantiates on the most common scalar types.
-template class VelocityKinematicsCache<double>;
-template class VelocityKinematicsCache<AutoDiffXd>;
-
-}  // namespace multibody
-}  // namespace drake
+DRAKE_DEFINE_CLASS_TEMPLATE_INSTANTIATIONS_ON_DEFAULT_SCALARS(
+    class drake::multibody::VelocityKinematicsCache)

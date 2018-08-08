ac7196e5369cf90d8ae3ca39c860c38d7a5908f2
amcastro-tri <alejandro.castro@tri.global>
2018-08-08 10:07:54 -0400

    Monitor normal separation velocities

--- multibody/multibody_tree/implicit_stribeck/implicit_stribeck_solver.cc
+++ multibody/multibody_tree/implicit_stribeck/implicit_stribeck_solver.cc
@@ -651,6 +651,7 @@ ComputationInfo ImplicitStribeckSolver<T>::SolveWithGuess(
   auto vt = variable_size_workspace_.mutable_vt();
   auto ft = variable_size_workspace_.mutable_ft();
   auto Delta_vt = variable_size_workspace_.mutable_Delta_vt();
+  VectorX<T> Delta_vn(nc_);
   auto& dft_dvt = variable_size_workspace_.mutable_dft_dvt();
   auto Gn = variable_size_workspace_.mutable_Gn();
   auto mu_vt = variable_size_workspace_.mutable_mu();
@@ -662,6 +663,8 @@ ComputationInfo ImplicitStribeckSolver<T>::SolveWithGuess(
   // Initialize vt_error to a value larger than tolerance so t_hat the solver at
   // least performs one iteration.
   T vt_error = 2 * vt_tolerance;
+
+  T vn_error = 2 * vt_tolerance;
 
   // Initialize iteration with the guess provided.
   v = v_guess;
@@ -685,7 +688,7 @@ ComputationInfo ImplicitStribeckSolver<T>::SolveWithGuess(
 
     // After the previous iteration, we allow updating ft above to have its
     // latest value before leaving.
-    if (vt_error < vt_tolerance) {
+    if (std::max(vt_error, vn_error) < vt_tolerance) {
       // Update generalized forces and return.
       tau_f = Jt.transpose() * ft;
       tau = tau_f + Jn.transpose() * fn;
@@ -731,8 +734,12 @@ ComputationInfo ImplicitStribeckSolver<T>::SolveWithGuess(
     // points.
     Delta_vt = Jt * Delta_v;
 
+    Delta_vn = Jn * Delta_v;
+
     // Convergence is monitored in the tangential velocities.
     vt_error = Delta_vt.norm();
+
+    vn_error = Delta_vn.norm();
 
     // Limit the angle change between vₜᵏ⁺¹ and vₜᵏ for all contact points.
     T alpha = CalcAlpha(vt, Delta_vt);

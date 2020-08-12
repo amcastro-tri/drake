d057a8c282d107bb53e7053d5935bcb597d0dac0
amcastro-tri <alejandro.castro@tri.global>
2020-08-12 16:08:20 -0400

    Splits ScratchSolver into its own files

---
+++ multibody/solvers/scratch_workspace.cc
@@ -0,0 +1,9 @@
+#include "drake/multibody/solvers/scratch_workspace.h"
+
+#include "drake/common/default_scalars.h"
+
+DRAKE_DEFINE_CLASS_TEMPLATE_INSTANTIATIONS_ON_DEFAULT_NONSYMBOLIC_SCALARS(
+    class ::drake::multibody::solvers::GrantScratchWorkspaceAccess)
+
+DRAKE_DEFINE_CLASS_TEMPLATE_INSTANTIATIONS_ON_DEFAULT_NONSYMBOLIC_SCALARS(
+    class ::drake::multibody::solvers::ScratchWorkspace)

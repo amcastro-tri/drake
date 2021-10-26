1cf5d0ef67005176bf2298ad729e8f1390c3d716
amcastro-tri <alejandro.castro@tri.global>
2021-10-26 15:58:24 -0400

    Adds sliding case

--- multibody/contact_solvers/test/sap_solver_test.cc
+++ multibody/contact_solvers/test/sap_solver_test.cc
@@ -15,6 +15,7 @@
 using Eigen::Matrix3d;
 using Eigen::Matrix4d;
 using Eigen::MatrixXd;
+using Eigen::Vector2d;
 using Eigen::Vector3d;
 using Eigen::Vector4d;
 using Eigen::VectorXd;
@@ -84,7 +85,7 @@ class PizzaSaverProblem {
     // Contact solver data.
     std::unique_ptr<SystemDynamicsData<double>> dynamics_data;
     std::unique_ptr<PointContactData<double>> contact_data;
-  };  
+  };
 
   PizzaSaverProblem(double dt, double mass, double radius, double mu, double k,
                     double taud)
@@ -109,6 +110,8 @@ class PizzaSaverProblem {
   double mass() const { return m_; }
 
   double radius() const { return R_; }
+
+  double rotational_inertia() const { return I_; }
 
   // Acceleration of gravity, m/s².
   double g() const { return g_; }
@@ -243,16 +246,26 @@ class PizzaSaverProblem {
     return MakeProblemData(q0, v0, tau);
   }
 
+  std::unique_ptr<ProblemData> MakeProblemData(const VectorXd& v0, double fx,
+                                               double Mz) const {
+    // Some arbitrary orientation. This particular case has symmetry of
+    // revolution (meaning the result is independent of angle theta).
+    const double theta = M_PI / 5;
+    const Vector4d q0(0.0, 0.0, 0.0, theta);
+    const Vector4d tau(fx, 0.0, -m_ * g_, Mz);
+    return MakeProblemData(q0, v0, tau);
+  }
+
  private:
-  double time_step_{NAN};  
+  double time_step_{NAN};
 
   // Pizza saver parameters.
-  double m_{1.0};   // Mass of the pizza saver.
-  double R_{1.0};   // Distance from COM to any contact point.    
+  double m_{1.0};           // Mass of the pizza saver.
+  double R_{1.0};           // Distance from COM to any contact point.
   double I_{R_ * R_ * m_};  // = 1.0 in this case.
 
   // Contact parameters:
-  double mu_{0};         // Friction coefficient.
+  double mu_{0};           // Friction coefficient.
   double stiffness_{NAN};  // Contact stiffness k.
   double taud_{NAN};       // Linear dissipation time scale: c = taud * k.
 
@@ -270,14 +283,6 @@ GTEST_TEST(PizzaSaver, NoAppliedTorque) {
   const double Mz = 0.0;
   const VectorXd v0 = VectorXd::Zero(problem.num_velocities);
   const auto data = problem.MakeProblemData(v0, Mz);
-  PRINT_VARn(data->M);
-  PRINT_VARn(data->J);
-  PRINT_VARn(data->v_star.transpose());
-  PRINT_VARn(data->phi0.transpose());
-  PRINT_VARn(data->vc0.transpose());
-  PRINT_VARn(data->stiffness.transpose());
-  PRINT_VARn(data->dissipation.transpose());
-  PRINT_VARn(data->mu.transpose());
 
   UnconstrainedPrimalSolverParameters params;
   params.rel_tolerance = 1e-6;
@@ -287,21 +292,14 @@ GTEST_TEST(PizzaSaver, NoAppliedTorque) {
   ContactSolverResults<double> result;
   // Arbitrary guess.
   VectorXd v_guess(problem.num_velocities);
-  v_guess << 0.0, 0.0, 0.0;  // TODO: make more interesting non-zero value.
+  v_guess << 1.0, 2.0, 3.0;  // Arbitrary non-zero initial guess.
 
-  EXPECT_EQ(sap.SolveWithGuess(dt, *data->dynamics_data, *data->contact_data,
+  ASSERT_EQ(sap.SolveWithGuess(dt, *data->dynamics_data, *data->contact_data,
                                v_guess, &result),
             ContactSolverStatus::kSuccess);
 
   // Expected generalized force.
   Vector4d tau_expected(0.0, 0.0, problem.mass() * problem.g(), 0.0);
-
-  PRINT_VARn(result.ft.transpose());
-  PRINT_VARn(result.fn.transpose());
-  PRINT_VARn(result.v_next.transpose());
-  PRINT_VARn(result.vn.transpose());
-  PRINT_VARn(result.vt.transpose());
-  PRINT_VARn(result.tau_contact.transpose());
 
   EXPECT_TRUE(CompareMatrices(result.v_next,
                               VectorXd::Zero(problem.num_velocities),
@@ -309,9 +307,8 @@ GTEST_TEST(PizzaSaver, NoAppliedTorque) {
   EXPECT_TRUE(CompareMatrices(result.tau_contact, tau_expected,
                               params.rel_tolerance,
                               MatrixCompareType::relative));
-  EXPECT_TRUE(CompareMatrices(result.ft,
-                              VectorXd::Zero(2 * problem.num_contacts),
-                              std::numeric_limits<double>::epsilon()));
+  EXPECT_TRUE(CompareMatrices(
+      result.ft, VectorXd::Zero(2 * problem.num_contacts), 4.0e-15));
   EXPECT_TRUE(CompareMatrices(
       result.fn,
       VectorXd::Constant(problem.num_contacts, tau_expected(2) / 3.0),
@@ -338,14 +335,6 @@ GTEST_TEST(PizzaSaver, Stiction) {
   const double Mz = 3.0;
   const VectorXd v0 = VectorXd::Zero(problem.num_velocities);
   const auto data = problem.MakeProblemData(v0, Mz);
-  PRINT_VARn(data->M);
-  PRINT_VARn(data->J);
-  PRINT_VARn(data->v_star.transpose());
-  PRINT_VARn(data->phi0.transpose());
-  PRINT_VARn(data->vc0.transpose());
-  PRINT_VARn(data->stiffness.transpose());
-  PRINT_VARn(data->dissipation.transpose());
-  PRINT_VARn(data->mu.transpose());
 
   UnconstrainedPrimalSolverParameters params;
   params.rel_tolerance = 1e-6;
@@ -358,9 +347,9 @@ GTEST_TEST(PizzaSaver, Stiction) {
   ContactSolverResults<double> result;
   // Arbitrary guess.
   VectorXd v_guess(problem.num_velocities);
-  v_guess << 0.0, 0.0, 0.0;  // TODO: make more interesting non-zero value.
+  v_guess << 1.0, 2.0, 3.0;  // Arbitrary non-zero initial guess.
 
-  EXPECT_EQ(sap.SolveWithGuess(dt, *data->dynamics_data, *data->contact_data,
+  ASSERT_EQ(sap.SolveWithGuess(dt, *data->dynamics_data, *data->contact_data,
                                v_guess, &result),
             ContactSolverStatus::kSuccess);
 
@@ -368,15 +357,7 @@ GTEST_TEST(PizzaSaver, Stiction) {
   const Vector4d tau_expected(0.0, 0.0, problem.mass() * problem.g(), -Mz);
 
   // Maximum expected slip. See Castro et al. 2021 for details.
-  const double max_slip_expected = params.sigma * dt * problem.g();
-  PRINT_VAR(max_slip_expected);
-
-  PRINT_VARn(result.ft.transpose());
-  PRINT_VARn(result.fn.transpose());
-  PRINT_VARn(result.v_next.transpose());
-  PRINT_VARn(result.vn.transpose());
-  PRINT_VARn(result.vt.transpose());
-  PRINT_VARn(result.tau_contact.transpose());
+  const double max_slip_expected = params.sigma * dt * problem.g();  
 
   EXPECT_TRUE(CompareMatrices(result.v_next.head<3>(), Vector3d::Zero(),
                               params.abs_tolerance));
@@ -393,123 +374,122 @@ GTEST_TEST(PizzaSaver, Stiction) {
   for (int i = 0; i < 3; ++i) {
     const double slip = result.vt.segment<2>(2 * i).norm();
     const double friction_force = result.ft.segment<2>(2 * i).norm();
-    const double normal_force = result.fn(i);    
+    const double normal_force = result.fn(i);
     EXPECT_LE(friction_force, mu * normal_force);
     EXPECT_NEAR(friction_force, friction_force_expected, params.rel_tolerance);
     EXPECT_LE(slip, max_slip_expected);
   }
 }
 
-#if 0
+// Test case with zero friction coefficient.
 GTEST_TEST(PizzaSaver, NoFriction) {
-  PizzaSaverProblem problem;
   const double dt = 0.01;
   const double mu = 0.0;
-  const double Mz = 3.0;
-  const auto data = problem.MakeProblemData(dt, mu, Mz);
-}
-#endif
+  const double k = 1.0e12;
+  const double taud = dt;
+  PizzaSaverProblem problem(dt, mu, k, taud);
 
-#if 0
-// This tests the solver when we apply a moment Mz about COM to the pizza saver.
-// If Mz < mu * m * g * R, the saver should be in stiction (that is, the sliding
-// velocity should be smaller than the regularization parameter). Otherwise the
-// saver will start sliding. For this setup the transition occurs at
-// M_transition = mu * m * g * R = 5.0
-TEST_F(PizzaSaver, SmallAppliedMoment) {
-  const double kTolerance = 10 * std::numeric_limits<double>::epsilon();
-  const double dt = 1.0e-3;  // time step in seconds.
+  const double fx = 1.0;
+  const double Mz = 0.0;
+  const VectorXd v0 = VectorXd::Zero(problem.num_velocities);
+  const auto data = problem.MakeProblemData(v0, fx, Mz);
+
+  UnconstrainedPrimalSolverParameters params;
+  params.rel_tolerance = 1e-6;
+  params.alpha = 0;  // Force SAP to use user stiffness.
+  UnconstrainedPrimalSolver<double> sap;
+  sap.set_parameters(params);
+  ContactSolverResults<double> result;
+  // Arbitrary guess.
+  VectorXd v_guess(problem.num_velocities);
+  v_guess << 1.0, 2.0, 3.0;  // Arbitrary non-zero initial guess.
+
+  ASSERT_EQ(sap.SolveWithGuess(dt, *data->dynamics_data, *data->contact_data,
+                               v_guess, &result),
+            ContactSolverStatus::kSuccess);
+
+  // Expected generalized force.
+  const Vector4d tau_expected(0.0, 0.0, problem.mass() * problem.g(), 0.0);
+
+  const double vx_expected = dt * fx / problem.mass();
+
+  EXPECT_TRUE(CompareMatrices(result.v_next,
+                              Vector4d(vx_expected, 0.0, 0.0, 0.0),
+                              params.rel_tolerance));
+  EXPECT_TRUE(CompareMatrices(result.tau_contact, tau_expected,
+                              params.rel_tolerance,
+                              MatrixCompareType::relative));
+  EXPECT_TRUE(CompareMatrices(result.ft,
+                              VectorXd::Zero(2 * problem.num_contacts),
+                              std::numeric_limits<double>::epsilon()));
+  EXPECT_TRUE(CompareMatrices(
+      result.fn,
+      VectorXd::Constant(problem.num_contacts, tau_expected(2) / 3.0),
+      params.rel_tolerance, MatrixCompareType::relative));
+  EXPECT_TRUE(CompareMatrices(result.vn, VectorXd::Zero(problem.num_contacts),
+                              params.abs_tolerance));
+  for (int i = 0; i < 3; ++i) {
+    const auto vt = result.vt.segment<2>(2 * i);
+    EXPECT_TRUE(CompareMatrices(vt, Vector2d(vx_expected, 0.0),
+                                params.rel_tolerance,
+                                MatrixCompareType::relative));
+  }
+}
+
+GTEST_TEST(PizzaSaver, Sliding) {
+  const double dt = 0.01;
   const double mu = 0.5;
+  const double k = 1.0e12;
+  const double taud = dt;
+  PizzaSaverProblem problem(dt, mu, k, taud);
 
-  // Some arbitrary orientation. This particular case has symmetry of
-  // revolution (meaning the result is independent of angle theta).
-  const double theta = M_PI / 5;
+  const double Mz = 6.0;
+  const double theta = M_PI / 5;  // Arbitrary orientation.
 
-  // External forcing.
-  const double Mz = 3.0;  // M_transition = 5.0
-  const Vector3<double> tau(0.0, 0.0, Mz);
+  const double weight = problem.mass() * problem.g();
+  const double fn_expected = weight / 3.0;
+  const double ft_expected = mu * fn_expected;
+  const double friction_torque_expected = 3.0 * ft_expected * problem.radius();
+  const double I = problem.rotational_inertia();
+  const double w_expected = dt * (Mz - friction_torque_expected) / I;
+  const double slip_expected = w_expected * problem.radius();
 
-  // Initial velocity.
-  const Vector3<double> v0 = Vector3<double>::Zero();
+  const double phi0 = dt * mu * slip_expected;
+  const Vector4d q0(0.0, 0.0, phi0, theta);
+  const VectorXd v0 = VectorXd::Zero(problem.num_velocities);
+  const Vector4d tau(0.0, 0.0, -weight, Mz);
 
-  SetProblem(v0, tau, mu, theta, dt);
+  const auto data = problem.MakeProblemData(q0, v0, tau);
 
-  TamsiSolverParameters parameters;  // Default parameters.
-  parameters.stiction_tolerance = 1.0e-6;
-  parameters.relative_tolerance = 1.0e-4;
-  solver_.set_solver_parameters(parameters);
+  UnconstrainedPrimalSolverParameters params;
+  params.rel_tolerance = 1e-6;
+  params.alpha = 0;  // Force SAP to use user stiffness.
+  UnconstrainedPrimalSolver<double> sap;
+  sap.set_parameters(params);
+  ContactSolverResults<double> result;
+  // Arbitrary guess.
+  VectorXd v_guess(problem.num_velocities);
+  v_guess << 1.0, 2.0, 3.0;  // Arbitrary non-zero initial guess.
 
-  TamsiSolverResult info = solver_.SolveWithGuess(dt, v0);
-  ASSERT_EQ(info, TamsiSolverResult::kSuccess);
+  ASSERT_EQ(sap.SolveWithGuess(dt, *data->dynamics_data, *data->contact_data,
+                               v_guess, &result),
+            ContactSolverStatus::kSuccess);
 
-  VectorX<double> tau_f = solver_.get_generalized_friction_forces();
+  // Expected generalized force.
+  const Vector4d tau_expected(0.0, 0.0, problem.mass() * problem.g(),
+                              -friction_torque_expected);
 
-  const auto& stats = solver_.get_iteration_statistics();
-
-  const double vt_tolerance =
-      // Dimensionless relative (to the stiction tolerance) tolerance.
-      solver_.get_solver_parameters().relative_tolerance *
-      solver_.get_solver_parameters().stiction_tolerance;
-  EXPECT_TRUE(stats.vt_residual() < vt_tolerance);
-
-  // For this problem we expect the x and y components of the forces due to
-  // friction to be zero.
-  EXPECT_NEAR(tau_f(0), 0.0, kTolerance);
-  EXPECT_NEAR(tau_f(1), 0.0, kTolerance);
-
-  // The moment due to friction should balance the applied Mz. However, it will
-  // take several time steps until tau_f balances Mz (eventually it will).
-  // Therefore, here we just sanity check that Mz is at least relatively close
-  // (to the value of Mz) to tau_f. In other words, with only a single time
-  // step, we are still accelerating towards the final steady state slip
-  // introduced by having a finite stiction tolerance.
-  EXPECT_NEAR(tau_f(2), -Mz, 5.0e-4);
-
-  const VectorX<double>& vt = solver_.get_tangential_velocities();
-  ASSERT_EQ(vt.size(), 2 * num_contacts);
-
-  // The problem has symmetry of revolution. Thus, for any rotation theta,
-  // the three tangential velocities should have the same magnitude.
-  const double v_slipA = vt.segment<2>(0).norm();
-  const double v_slipB = vt.segment<2>(2).norm();
-  const double v_slipC = vt.segment<2>(4).norm();
-  EXPECT_NEAR(v_slipA, v_slipB, kTolerance);
-  EXPECT_NEAR(v_slipA, v_slipC, kTolerance);
-  EXPECT_NEAR(v_slipC, v_slipB, kTolerance);
-
-  // For this case where Mz < M_transition, we expect stiction (slip velocities
-  // are smaller than the regularization parameter).
-  EXPECT_LT(v_slipA, parameters.stiction_tolerance);
-  EXPECT_LT(v_slipB, parameters.stiction_tolerance);
-  EXPECT_LT(v_slipC, parameters.stiction_tolerance);
-
-  const VectorX<double>& v = solver_.get_generalized_velocities();
-  ASSERT_EQ(v.size(), num_velocities);
-
-  // For this problem we expect the translational velocities to be zero.
-  EXPECT_NEAR(v(0), 0.0, kTolerance);
-  EXPECT_NEAR(v(1), 0.0, kTolerance);
-
-  // Compute the Newton-Raphson Jacobian of the residual J = ∇ᵥR using the
-  // solver's internal implementation.
-  MatrixX<double> J = TamsiSolverTester::CalcJacobian(solver_, v, dt);
-
-  // Compute the same Newton-Raphson Jacobian of the residual J = ∇ᵥR but with
-  // a completely separate implementation using automatic differentiation.
-  const double v_stiction = parameters.stiction_tolerance;
-  const double epsilon_v = v_stiction * parameters.relative_tolerance;
-  MatrixX<double> J_expected = test::CalcOneWayCoupledJacobianWithAutoDiff(
-      M_, Jn_, Jt_, p_star_, mu_, fn_, dt, v_stiction, epsilon_v, v);
-
-  // We use a tolerance scaled by the norm and size of the matrix.
-  const double J_tolerance = J_expected.rows() * J_expected.norm() *
-                             std::numeric_limits<double>::epsilon();
-
-  // Verify the result.
-  EXPECT_TRUE(
-      CompareMatrices(J, J_expected, J_tolerance, MatrixCompareType::absolute));
+  EXPECT_GT(result.v_next(3), 0.0);  // It's accelerating.
+  EXPECT_NEAR(tau_expected.head<2>().norm(), 0.0, params.abs_tolerance);
+  EXPECT_GT(std::abs(tau_expected(2)), 0.0);
+  EXPECT_LT(std::abs(tau_expected(3)), Mz);
+  for (int i = 0; i < 3; ++i) {
+    const double friction_force = result.ft.segment<2>(2 * i).norm();
+    const double normal_force = result.fn(i);
+    EXPECT_NEAR(friction_force, mu * normal_force,
+                std::numeric_limits<double>::epsilon());
+  }
 }
-#endif
 
 }  // namespace internal
 }  // namespace contact_solvers

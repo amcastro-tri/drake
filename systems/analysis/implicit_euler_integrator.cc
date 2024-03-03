amcastro-tri/drake

commit a7fe4099c12d67d0c2ddef508dc9a67fa725a64f

First attempt at making IE truly a fixed step integrator.

systems/analysis/implicit_euler_integrator.cc
@@ -5,6 +5,7 @@
 #include <limits>
 #include <stdexcept>
 #include <utility>
+#include <iostream>
 
 #include "drake/common/drake_assert.h"
 #include "drake/common/fmt_eigen.h"
@@ -121,6 +122,8 @@ bool ImplicitEulerIntegrator<T>::StepAbstract(
   DRAKE_LOGGER_DEBUG("StepAbstract() entered for t={}, h={}, trial={}",
       t0, h, trial);
 
+  std::cout << "StepAbstract()\n";      
+
   // Start from the guess.
   *xtplus = xtplus_guess;
   DRAKE_LOGGER_DEBUG("Starting state: {}", fmt_eigen(xtplus->transpose()));
@@ -211,6 +214,8 @@ bool ImplicitEulerIntegrator<T>::StepImplicitEuler(const T& t0, const T& h,
     const VectorX<T>& xt0, VectorX<T>* xtplus) {
   DRAKE_LOGGER_DEBUG("StepImplicitEuler(h={}) t={}", h, t0);
 
+  std::cout << "StepImplicitEuler()\n";
+
   // Use the current state as the candidate value for the next state.
   // [Hairer 1996] validates this choice (p. 120).
   const VectorX<T>& xtplus_guess = xt0;
@@ -225,6 +230,8 @@ bool ImplicitEulerIntegrator<T>::StepImplicitEulerWithGuess(
   using std::abs;
 
   DRAKE_LOGGER_DEBUG("StepImplicitEulerWithGuess(h={}) t={}", h, t0);
+
+  std::cout << "StepImplicitEulerWithGuess()\n";
 
   // Set g for the implicit Euler method.
   Context<T>* context = this->get_mutable_context();
@@ -247,6 +254,8 @@ bool ImplicitEulerIntegrator<T>::StepHalfSizedImplicitEulers(
   using std::abs;
 
   DRAKE_LOGGER_DEBUG("StepHalfSizedImplicitEulers(h={}) t={}", h, t0);
+
+  std::cout << "StepHalfSizedImplicitEulers()\n";
 
 
   // Store statistics before calling StepAbstract(). The difference between
@@ -394,6 +403,8 @@ bool ImplicitEulerIntegrator<T>::AttemptStepPaired(const T& t0, const T& h,
   DRAKE_ASSERT(xtplus_ie != nullptr);
   DRAKE_ASSERT(xtplus_hie != nullptr);
 
+  std::cout << "AttemptStepPaired()\n";
+
   // Compute the derivative at time and state (t0, x(t0)). NOTE: the derivative
   // is calculated at this point (early on in the integration process) in order
   // to reuse the derivative evaluation, via the cache, from the last
@@ -406,6 +417,13 @@ bool ImplicitEulerIntegrator<T>::AttemptStepPaired(const T& t0, const T& h,
     DRAKE_LOGGER_DEBUG("Implicit Euler approach did not converge for "
         "step size {}", h);
     return false;
+  } else {
+    // Single step succeeded, no need to compute error estimation.
+    // We take our single step as the solution, unlike the error-controlled
+    // version that uses the two half-steps as the solution.
+    Context<T>* context = this->get_mutable_context();
+    context->SetTimeAndContinuousState(t0 + h, *xtplus_ie);
+    return true;
   }
 
   if (!use_implicit_trapezoid_error_estimation_) {
@@ -458,6 +476,8 @@ bool ImplicitEulerIntegrator<T>::DoImplicitIntegratorStep(const T& h) {
   Context<T>* context = this->get_mutable_context();
   const T t0 = context->get_time();
   DRAKE_LOGGER_DEBUG("IE DoStep(h={}) t={}", h, t0);
+
+  std::cout << "\nDoImplicitIntegratorStep()\n";
 
   xt0_ = context->get_continuous_state().CopyToVector();
   xtplus_ie_.resize(xt0_.size());
@@ -546,7 +566,15 @@ bool ImplicitEulerIntegrator<T>::DoImplicitIntegratorStep(const T& h) {
   }
 
   // Compute and update the error estimate.
+  // N.B. This right now is allocating err_est_vec_ to the proper size.
   err_est_vec_ = xtplus_ie_ - xtplus_hie_;
+
+  if (this->get_fixed_step_mode()) {
+    // We should not be doing an error estimate in fixed step mode.
+    // TODO: consider setting this to NAN at initialization or something like
+    // that.
+    err_est_vec_.setConstant(NAN);
+  }
 
   // Update the caller-accessible error estimate.
   this->get_mutable_error_estimate()->get_mutable_vector().

systems/analysis/implicit_euler_integrator.h
@@ -339,26 +339,40 @@ class ImplicitEulerIntegrator final : public ImplicitIntegrator<T> {
     // small half-sized steps are propagated and the large step is the error
     // estimator, so we report error estimator stats by subtracting those of
     // the small half-sized steps from the total statistics.
-    return use_implicit_trapezoid_error_estimation_
-               ? itr_statistics_.num_function_evaluations
-               : (this->get_num_derivative_evaluations() -
-                  hie_statistics_.num_function_evaluations);
+    if (this->get_fixed_step_mode()) {
+      // this should be zero.
+      return use_implicit_trapezoid_error_estimation_
+                 ? itr_statistics_.num_function_evaluations
+                 : hie_statistics_.num_function_evaluations;
+    } else {
+      return use_implicit_trapezoid_error_estimation_
+                 ? itr_statistics_.num_function_evaluations
+                 : (this->get_num_derivative_evaluations() -
+                    hie_statistics_.num_function_evaluations);
+    }
   }
 
   int64_t do_get_num_error_estimator_derivative_evaluations_for_jacobian()
       const final {
     // See the above comments in
     // do_get_num_error_estimator_derivative_evaluations().
-    return use_implicit_trapezoid_error_estimation_
-               ? itr_statistics_.num_jacobian_function_evaluations
-               : (this->get_num_derivative_evaluations_for_jacobian() -
-                  hie_statistics_.num_jacobian_function_evaluations);
+    if (this->get_fixed_step_mode()) {
+      return use_implicit_trapezoid_error_estimation_
+                 ? itr_statistics_.num_jacobian_function_evaluations
+                 : hie_statistics_.num_jacobian_function_evaluations;
+    } else {
+      return use_implicit_trapezoid_error_estimation_
+                 ? itr_statistics_.num_jacobian_function_evaluations
+                 : (this->get_num_derivative_evaluations_for_jacobian() -
+                    hie_statistics_.num_jacobian_function_evaluations);
+    }
   }
 
   int64_t do_get_num_error_estimator_newton_raphson_iterations()
       const final {
     // See the above comments in
     // do_get_num_error_estimator_derivative_evaluations().
+    if (this->get_fixed_step_mode()) return 0;
     return use_implicit_trapezoid_error_estimation_
                ? itr_statistics_.num_nr_iterations
                : (this->get_num_newton_raphson_iterations() -
@@ -368,6 +382,7 @@ class ImplicitEulerIntegrator final : public ImplicitIntegrator<T> {
   int64_t do_get_num_error_estimator_jacobian_evaluations() const final {
     // See the above comments in
     // do_get_num_error_estimator_derivative_evaluations().
+    if (this->get_fixed_step_mode()) return 0;
     return use_implicit_trapezoid_error_estimation_
                ? itr_statistics_.num_jacobian_reforms
                : (this->get_num_jacobian_evaluations() -
@@ -378,6 +393,7 @@ class ImplicitEulerIntegrator final : public ImplicitIntegrator<T> {
       const final {
     // See the above comments in
     // do_get_num_error_estimator_derivative_evaluations().
+    if (this->get_fixed_step_mode()) return 0;
     return use_implicit_trapezoid_error_estimation_
                ? itr_statistics_.num_iter_factorizations
                : (this->get_num_iteration_matrix_factorizations() -

commit 652d31dfec301c7bf34b38f845c43b44452121c3

Fixes bug introduced in a7fe4099c1 to make IE 'fully fixed step'

systems/analysis/implicit_euler_integrator.cc
@@ -417,7 +417,7 @@ bool ImplicitEulerIntegrator<T>::AttemptStepPaired(const T& t0, const T& h,
     DRAKE_LOGGER_DEBUG("Implicit Euler approach did not converge for "
         "step size {}", h);
     return false;
-  } else {
+  } else if (this->get_fixed_step_mode()) {
     // Single step succeeded, no need to compute error estimation.
     // We take our single step as the solution, unlike the error-controlled
     // version that uses the two half-steps as the solution.